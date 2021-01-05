f6eb6d5c635e45179b78bf7fa2f9070b51cd5ba8
paria rezaee fard <paria_rezaee4224@yahoo.com>
2021-01-05 21:35:29 +0330

    tree structure change

---
+++ src/pages/main-page/index.jsx
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import '../App.css';
+import Header from '../../components/layout/header';
+
+function MainPage() {
+  return (
+    <div className="main-page">
+      <Header />
+    </div>
+  );
+}
+
+export default MainPage;
