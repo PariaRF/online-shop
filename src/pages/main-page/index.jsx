b04dcc05e7e44064d3a82d20feace31b704b7b45
paria rezaee fard <paria_rezaee4224@yahoo.com>
2021-01-07 18:46:31 +0330

    convert function component to class component

--- src/pages/main-page/index.jsx
+++ src/pages/main-page/index.jsx
@@ -1,19 +1,22 @@
+import React, { Component } from "react";
 import NavBar from '../../components/common/nav-bar';
 import ProductList from '../../components/common/product-list';
 import Header from '../../components/layout/header';
 import styles from "./styles.module.css";
 
-function MainPage() {
-  return (
-    <div>
-      <Header>
-        <NavBar />
-      </Header>
-      <div className={styles.mainPage}>
-      <ProductList />
+class MainPage extends Component{
+  render() {
+    return (
+      <div>
+        <Header>
+          <NavBar />
+        </Header>
+        <div className={styles.mainPage}>
+          <ProductList />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default MainPage;
