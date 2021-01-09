1f13fde0daff159907a4b704c177f38e2e43700f
paria rezaee fard <paria_rezaee4224@yahoo.com>
2021-01-09 11:58:31 +0330

    set needauth for components

--- src/router/utils/routes.js
+++ src/router/utils/routes.js
@@ -12,30 +12,35 @@ const routes = [
         path: pathes.INDEX,
         component: MainPage,
         exact: true,
+        needAuth: false,
     },
     {
         name: "Profile",
         path: pathes.PROFILE,
         component: ProfilePage,
         exact: true,
+        needAuth: true,
     },
     {
         name: "PRODUCT",
         path: pathes.PRODUCT,
         component: ProductPage,
         exact: true,
+        needAuth: false,
     },
     {
         name: "FORBIDDEN",
         path: pathes.FORBIDDEN,
         component: ForbiddenPage,
         exact: true,
+        needAuth: false
     },
     {
         name: "NOT_FOUND",
         path: pathes.NOT_FOUND,
         component: NotFoundPage,
         exact: true,
+        needAuth: false,
     },
 ]
 
